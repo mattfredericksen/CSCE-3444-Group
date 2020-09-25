8fbf5c49c941accdd36938b7125e9227b42d41d6
mattfredericksen <mfred98@gmail.com>
2020-09-25 14:47:52 -0500

    Initial commit

---
+++ sensor_api/test_sensors_api.py
@@ -0,0 +1,28 @@
+import pytest
+import sensor_api
+import json
+
+
+@pytest.fixture
+def response():
+    sensor_api.app.config['TESTING'] = True
+    with sensor_api.app.test_client() as client:
+        yield client.get('/')
+
+
+def test_response_is_200(response):
+    assert response.status_code == 200
+
+
+def test_content_is_json(response):
+    assert response.content_type == 'application/json'
+
+
+def test_sensor_count_is_3(response):
+    data = json.loads(response.data)
+    assert len(data) == 3
+
+
+def test_all_sensors_reading(response):
+    data = json.loads(response.data)
+    assert all(value for value in data.values())
