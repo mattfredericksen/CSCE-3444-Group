745d9537a0ce7bce6605a12794c1ba078742d324
mattfredericksen <mfred98@gmail.com>
2020-09-28 13:52:41 -0500

    Added support for Prometheus scraping

--- sensor_api/sensor_api.py
+++ sensor_api/sensor_api.py
@@ -3,14 +3,25 @@ This module launches a Flask app REST api
 for retrieving temperature and pressure sensor data
 """
 
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client.exposition import make_wsgi_app
 
 from flask import Flask
 from flask_restful import Resource, Api
+from prometheus_client import Gauge
 
 from sensors import sensor_list
 
 app = Flask(__name__)
 api = Api(app)
+
+# Errors might happen here after a hot-reload in debug mode
+#     ValueError: Duplicated timeseries in CollectorRegistry
+gauges = []
+for s in sensor_list:
+    g = Gauge(s.name, f'{s.name} sensor')
+    g.set_function(s.read)
+    gauges.append(s)
 
 # TODO: consider switching from HTTP to HTTPS
 #       and adding authentication
@@ -24,6 +35,11 @@ class Sensors(Resource):
 
 
 api.add_resource(Sensors, '/')
+
+# Add prometheus wsgi middleware to route /metrics requests
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
 
 if __name__ == '__main__':
     # we will remove debug mode in final product
