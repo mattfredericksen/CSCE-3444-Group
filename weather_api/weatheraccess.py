4ac52c3a7b6cd124deeb83963cc77ff6316ac5ae
mattfredericksen <mfred98@gmail.com>
2020-10-12 17:09:06 -0500

    Minor format changes. Changed response units to Fahrenheit.

--- weather_api/weatheraccess.py
+++ weather_api/weatheraccess.py
@@ -1,14 +1,20 @@
-#	Returns current weather outside in kelvin
-#	Access to 60 per minute, or 22 per minute if operating consistently
+# Returns current weather outside in kelvin
+# Access to 60 per minute, or 22 per minute if operating consistently
 
-#	Celcius = kelvin - 273.15
-#	Fahrenheit = (Celcius*9/5) + 32
+# Celcius = kelvin - 273.15
+# Fahrenheit = (Celcius*9/5) + 32
 
 import requests
 
+
 def get_weather():
+    url = 'http://api.openweathermap.org/data/2.5/weather'
     zipcode = '75028'
-    response = requests.get("http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + "&APPID=f2d40f5cd4f606abecf7872b7a9ebc72")
-    
-    print(response.json()['main']['temp'])
+    app_id = 'f2d40f5cd4f606abecf7872b7a9ebc72'
+
+    response = requests.get(f'{url}?zip={zipcode}&APPID={app_id}')
+
+    # for now, respond with Fahrenheit
+    degrees_k = response.json()['main']['temp']
+    return round(((degrees_k - 273.15) * 9/5) + 32, 1)
 
