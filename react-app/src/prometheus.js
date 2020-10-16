a30e019df54673b28de7691b9afd768cbb404658
mattfredericksen <mfred98@gmail.com>
2020-10-16 13:44:49 -0500

    Added comments.

--- react-app/src/prometheus.js
+++ react-app/src/prometheus.js
@@ -1,5 +1,7 @@
 export const api = "http://localhost:9090/api/v1/query?";
 
+// this function takes a response from a prometheus query
+// and returns just the values we want.
 export function extract_sensors(response) {
     return (
         response.data.result.map((metric) => {
