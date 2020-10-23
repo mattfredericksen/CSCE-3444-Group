fb22aa9aac45918b5a80d1f1cf927e6397e045ee
mattfredericksen <mfred98@gmail.com>
2020-10-22 23:44:22 -0500

    Experimenting with Mui date picker.

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import LiveView from './LiveView';
+import MomentUtils from '@date-io/moment';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <LiveView />
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+          <LiveView />
+      </MuiPickersUtilsProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
