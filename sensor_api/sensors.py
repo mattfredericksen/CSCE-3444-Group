a3db4c971fd465d58ec789d79e46a726ba9856a3
mattfredericksen <mfred98@gmail.com>
2020-11-02 21:09:35 -0600

    Updated sensor read() methods with real data

--- sensor_api/sensors.py
+++ sensor_api/sensors.py
@@ -4,7 +4,15 @@ them accessible through the `get_sensors()` function.
 """
 from prometheus_client import Gauge
 from weatheraccess import get_weather
-from random import uniform, choice
+
+import board
+import busio
+from adafruit_mcp9808 import MCP9808
+from adafruit_ads1x15.ads1115 import ADS1115, P0
+from adafruit_ads1x15.analog_in import AnalogIn
+
+
+i2c = busio.I2C(board.SCL, board.SDA)
 
 
 class Sensor(Gauge):
@@ -15,64 +23,73 @@ class Sensor(Gauge):
         raise NotImplementedError
 
     def collect(self):
-        # overriding this function allows sensors to refresh
+        # this override allows sensors to refresh
         # immediately before yielding metrics to Prometheus
         self.set(self.read())
         return super().collect()
 
 
 class TemperatureSensor(Sensor):
+    def __init__(self, addr, name, *args, **kwargs):
+        super().__init__(name, *args, **kwargs)
+        self.device = MCP9808(i2c, address=addr)
+
     def read(self):
-        # research in progress for connecting sensors to RPi
-        pass
+        # currently reads Celsius
+        return self.device.temperature
 
 
 class PressureSensor(Sensor):
+    def __init__(self, addr, name, *args, **kwargs):
+        super().__init__(name, *args, **kwargs)
+        self.device = AnalogIn(ADS1115(i2c, address=addr), P0)
+
     def read(self):
-        # research in progress for connecting sensors to RPi
-        pass
+        return self.device.value
 
 
 class StateSensor(Sensor):
-    # Rather than recording a count of the times the system has cycled,
-    # this sensor just checks the current on/off state.
-    # Later, we can use this function for creating the count:
-    # https://prometheus.io/docs/prometheus/latest/querying/functions/#changes
+    def __init__(self, addr, name, *args, **kwargs):
+        super().__init__(name, *args, **kwargs)
+        self.device = AnalogIn(ADS1115(i2c, address=addr), P0)
 
     def read(self):
-        # research if progress for checking on/off state
-        pass
+        # When system is running, voltage should be ~3.
+        # When system is not running, noise may cause
+        # voltage to rise above 0, but not more than 1
+        return False if self.device.voltage < 1 else True
 
 
-# TODO: discover and process sensors here
+# Must be defined after class definitions.
+# Empty dicts in case kwargs need to be added in the future.
+device_addr_map = {
+    0x18: (TemperatureSensor, ('incoming_air',), {}),
+    0x19: (TemperatureSensor, ('outgoing_air',), {}),
+    0x48: (PressureSensor, ('air_pressure',), {}),
+    0x49: (StateSensor, ('is_on',), {}),
+}
+
+
 def get_sensors():
     """This function returns a list of objects which
     can be detected by Prometheus's metric scraping.
     It should only be called once to avoid duplicating
     metrics in the collector registry.
     """
+
+    addresses = i2c.scan()
+    if len(addresses) != len(device_addr_map):
+        print(f'Expected addresses: {list(device_addr_map.keys())}')
+        print(f'Discovered addresses: {addresses}')
+
     sensor_list = []
+    for addr in i2c.scan():
+        if addr in device_addr_map:
+            cls, args, kwargs = device_addr_map[addr]
+            sensor_list.append(cls(addr, *args, **kwargs))
 
-    # this gauge is fully functional
-    s = Gauge('outside_air', 'Outside air temperature')
+    s = Gauge('outside_air', 'Weather API temperature')
     s.set_function(lambda: get_weather('76201'))
-    sensor_list.append(s)
-
-    # for now, mock the remaining sensors with random data
-    s = TemperatureSensor('incoming_air')
-    s.read = lambda: round(uniform(70, 80), 1)
-    sensor_list.append(s)
-
-    s = TemperatureSensor('outgoing_air')
-    s.read = lambda: round(uniform(60, 70), 1)
-    sensor_list.append(s)
-
-    s = PressureSensor('pressure')
-    s.read = lambda: round(uniform(.8, 1), 2)
-    sensor_list.append(s)
-
-    s = StateSensor('is_on')
-    s.read = lambda: choice([True, False])
     sensor_list.append(s)
 
     return sensor_list
