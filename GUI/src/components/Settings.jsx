af9d228a8bf934363d41fed144c0d2fdb5150690
LuisMR001 <70413606+LuisMR001@users.noreply.github.com>
2020-11-11 08:26:26 -0600

    Adding text to Setting page
    The format will change later, just using this for now as a place holder. I am unsure on how change email will work.

--- GUI/src/components/Settings.jsx
+++ GUI/src/components/Settings.jsx
@@ -1,16 +1,49 @@
 import React, { Component } from "react";
 
+    const divStyle = {
+        color: 'orange',
+        textAlign: "center",
+    };
+
 class Settings extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { emailName: 'Enter new email'};
+    }
+    myChangeHandler = (event) => {
+        this.setState({emailName: event.target.value});
+    }
     state = { isOpen: false };
 
     render() {
         return(
-            <div>
-                <style>{"h3 {color:white}"}</style>
-                <h3 className="m-2">
+            <form>
+                <h1 style={divStyle}>
                     Settings
-                </h3>
-            </div>
+                </h1>
+                <h4 style={divStyle}>
+                    Change Email
+                </h4>
+                <h4 style={divStyle}>
+                    Current Email:
+                    <h5 style={divStyle}>
+                        SomeOne@mail.com
+                    </h5>
+                </h4>
+                <h4 style={divStyle}>
+                    New Email:
+                    <input
+                        type="text"
+                        onChange={this.myChangeHandler}
+                    />
+                </h4>
+                <h4 style={divStyle}>
+                    New Email
+                    <h4 style={divStyle}>
+                        {this.state.emailName}
+                    </h4>
+                </h4>
+            </form>
         );
     }
 }
