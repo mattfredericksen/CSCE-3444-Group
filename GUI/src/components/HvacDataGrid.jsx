0be4c39033b321a1ed58840338b2c3eff27410fb
mattfredericksen <mfred98@gmail.com>
2020-11-14 00:26:14 -0600

    Moved `columns` object creation outside of `HvacDataGrid()` to avoid recreating the same object at every call. Added `loading` state to enhance UI and ensure re-rendering.

--- GUI/src/components/HvacDataGrid.jsx
+++ GUI/src/components/HvacDataGrid.jsx
@@ -4,34 +4,30 @@ import { DataGrid } from "@material-ui/data-grid";
 import { Queries, rowsFromQueries, getRangeOffset } from "./prometheus";
 
 
+const columns = [{
+        field: 'Timestamp',
+        width: 200,
+        valueFormatter: ({value}) => moment.unix(value),
+    },
+    ...Queries.map((q) => ({field: q.name, ...q.colSettings}))
+];
+
 export default function HvacDataGrid(props) {
     const { startDate, endDate } = props;
     const { range, offset } = getRangeOffset(startDate, endDate);
 
     const [rows, setRows] = useState([]);
-    // const [cols, setCols] = useState([{
-    //         field: 'Timestamp',
-    //         width: 200,
-    //     }, ...Queries.map(q => ({field: q.name}))
-    // ]);
+    const [loading, setLoading] = useState(null);
 
     useEffect(() => {
         if (range <= 0 || offset < 0) return;
+        setLoading(true);
         rowsFromQueries(range, offset)
-            .then(rows => setRows(rows));
+            .then(rows => {
+                setRows(rows);
+                setLoading(false);
+            });
     }, [startDate, endDate]);
 
-    const columns = [{
-        field: 'Timestamp',
-        width: 200,
-        valueFormatter: ({value}) => moment.unix(value),
-    }];
-    for (const { name, colSettings } of Queries) {
-        columns.push({
-            field: name,
-            ...colSettings,
-        });
-    }
-
-    return <DataGrid rows={rows} columns={columns} autoPageSize autoHeight />;
+    return <DataGrid rows={rows} columns={columns} loading={loading} autoPageSize autoHeight />;
 }
