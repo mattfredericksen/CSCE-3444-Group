f02bf89d8f8b405babda1b20bdbd89b97f003296
mattfredericksen <mfred98@gmail.com>
2020-11-19 19:58:18 -0600

    Moved MuiPickersUtilsProvider so it only wraps the component that needs it.

--- GUI/src/components/ConfiguredDateTimePicker.jsx
+++ GUI/src/components/ConfiguredDateTimePicker.jsx
@@ -1,20 +1,24 @@
-import {DateTimePicker} from "@material-ui/pickers";
+import {DateTimePicker, MuiPickersUtilsProvider} from "@material-ui/pickers";
 import {InputAdornment} from "@material-ui/core";
 import EventIcon from "@material-ui/icons/Event";
 import React from "react";
+import MomentUtils from "@date-io/moment";
 
 export default function ConfiguredDateTimePicker(props) {
     return (
-        <DateTimePicker
-            disableFuture fullWidth inputVariant={'outlined'}
-            InputProps={{
-                startAdornment: (
-                    <InputAdornment position="start">
-                        <EventIcon />
-                    </InputAdornment>
-                ),
-            }}
-            {...props}
-        />
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+            <DateTimePicker
+                disableFuture fullWidth
+                inputVariant={'outlined'} minutesStep={5}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <EventIcon />
+                        </InputAdornment>
+                    ),
+                }}
+                {...props}
+            />
+        </MuiPickersUtilsProvider>
     );
 }
--- GUI/src/index.js
+++ GUI/src/index.js
@@ -4,13 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-import MomentUtils from "@date-io/moment";
 
 ReactDOM.render(
-    <MuiPickersUtilsProvider utils={MomentUtils}>
-        <App />
-    </MuiPickersUtilsProvider>,
+        <App />,
     document.getElementById("root")
 );
 
