297e90ed3891286afd2e074d5e12464a2aafc8e1
mattfredericksen <mfred98@gmail.com>
2020-11-19 23:49:21 -0600

    Major refactor, moved all input fields from the Historical view into a distinct component.

--- GUI/src/components/Historical.jsx
+++ GUI/src/components/Historical.jsx
@@ -1,46 +1,30 @@
 import React, {Component} from "react";
 import moment from "moment";
-import DateTimePicker from "./ConfiguredDateTimePicker";
 import {Container, Grid, Snackbar} from "@material-ui/core";
 import Alert from '@material-ui/lab/Alert';
 
-import {oldestSample} from "./prometheus";
+import HvacInputGrid from "./HvacInputGrid";
 import HvacDataGrid from "./HvacDataGrid";
-import {LengthOfTimeInput, UnitOfTimeSelect} from "./InputFields";
 
 class Historical extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            minDate: moment(),
             startDate: moment(),
             endDate: moment(),
-            repeat: {
-                disabled: false,
-                duration: '1', durUnit: 'day',
-                resolution: '1', resUnit: 'day',
-            },
-            repeatParams: {duration: null, resolution: null},
+            resolution: "1d",
+            duration: "1d",
             alert: {message: "", severity: "error"},
         };
 
-        this.update = this.update.bind(this);
-        this.setStartDate = this.setStartDate.bind(this);
-        this.setEndDate = this.setEndDate.bind(this);
+        this.setState = this.setState.bind(this);
         this.setAlert = this.setAlert.bind(this);
         this.closeAlert = this.closeAlert.bind(this);
-        this.setRepeatParams = this.setRepeatParams.bind(this);
-
-        // set the earliest date available to the date-pickers
-        oldestSample()
-            .then(res => this.setState({minDate: res}))
-            .catch(e => this.setAlert("Unable to retrieve oldest sample", "warning"));
-    }
+}
 
     setAlert(message="", severity="error") {
         // call with no arguments to remove alert
-
         this.setState({
             alert: {
                 severity: severity,
@@ -49,105 +33,32 @@ class Historical extends Component {
         });
     }
 
-    update() {
-        const { startDate, endDate, alert } = this.state;
-        console.log(`update: \n\t${startDate} \n\t${endDate}`);
-
-        if (startDate.isSameOrAfter(endDate)) {
-            this.setAlert("Start date must be earlier than end date");
-        } else if (endDate.isAfter(moment())) {
-            this.setAlert("Cannot query the future");
-        } else if (alert.message) {
-            this.setAlert();
-        }
-    }
-
-    setStartDate(date) {
-        this.setState({startDate: date}, this.update);
-    }
-
-    setEndDate(date) {
-        this.setState({endDate: date}, this.update);
-    }
-
     closeAlert(event, reason) {
         if (reason !== 'clickaway') this.setAlert();
     }
 
-    setRepeat(id, event) {
-        const { repeat } = this.state;
-        this.setState({repeat: {...repeat, [id]: event.target.value}},
-            this.setRepeatParams);
-    }
-
-    setRepeatParams() {
-        const { disabled, duration, durUnit, resolution, resUnit } = this.state.repeat;
-        let params;
-
-        if (disabled) {
-            params = {duration: null, resolution: null};
-        } else {
-            const dur = moment.duration(duration, durUnit);
-            const res = moment.duration(resolution, resUnit);
-            params = {
-                duration: `${dur.asDays()}d`,
-                resolution: `${res.asDays()}d`
-            }
-        }
-        this.setState({repeatParams: params});
-    }
-
-
     render() {
-        const { minDate, startDate, endDate, repeat, repeatParams, alert } = this.state;
+        const { startDate, endDate, resolution, duration, alert } = this.state;
 
         return (
             <Container maxWidth={"md"}>
-                <Grid container spacing={2} justify={'center'}>
+                <Grid container spacing={2} justify={'center'} alignItems={'center'}>
                     <Grid item xs={8}>
-                        <h3 style={{textAlign: 'center', color: 'white'}}>Historical</h3>
+                        <h3 style={{textAlign: 'center', color: 'white'}}>
+                            {"Historical"}
+                        </h3>
                     </Grid>
 
-                    <Grid item xs={9} sm={6}>
-                        <DateTimePicker
-                            label={"Start Date"} value={startDate}
-                            onChange={this.setStartDate} minDate={minDate}
-                        />
-                    </Grid>
-                    <Grid item xs={9} sm={6}>
-                        <DateTimePicker
-                            label={"End Date"} value={endDate}
-                            onChange={this.setEndDate} minDate={minDate}
-                        />
-                    </Grid>
-
-                    <Grid item xs={5} sm={3}>
-                        <LengthOfTimeInput
-                            label={"Repeat every"}
-                            onChange={this.setRepeat.bind(this, 'resolution')}
-                        />
-                    </Grid>
-                    <Grid item xs={5} sm={3}>
-                        <UnitOfTimeSelect
-                            onChange={this.setRepeat.bind(this, 'resUnit')}
-                            value={repeat.resUnit} plural={repeat.resolution !== '1'}/>
-                    </Grid>
-                    <Grid item xs={5} sm={3}>
-                    <LengthOfTimeInput
-                        label={"For the last"}
-                        onChange={this.setRepeat.bind(this, 'duration')}
+                    <HvacInputGrid
+                        onChange={this.setState} setAlert={this.setAlert}
+                        startDate={startDate} endDate={endDate}
                     />
-                    </Grid>
-                    <Grid item xs={5} sm={3}>
-                        <UnitOfTimeSelect
-                            onChange={this.setRepeat.bind(this, 'durUnit')}
-                            value={repeat.durUnit} plural={repeat.duration !== '1'}/>
-                    </Grid>
 
                     <Grid item xs={12}>
                         <HvacDataGrid
                             startDate={startDate} endDate={endDate}
-                            errorCallback={this.setAlert} {...repeatParams}
+                            duration={duration} resolution={resolution}
+                            setAlert={this.setAlert}
                         />
                     </Grid>
                 </Grid>
