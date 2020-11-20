124a8d3f4f0f53294293b54386c6215ad838634e
mattfredericksen <mfred98@gmail.com>
2020-11-19 20:00:08 -0600

    Added alert callback for error handling to HvacDataGrid. Closes #20.

--- GUI/src/components/HvacDataGrid.jsx
+++ GUI/src/components/HvacDataGrid.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import moment from "moment";
-import { DataGrid } from "@material-ui/data-grid";
-import { Queries, rowsFromQueries, getRangeOffset } from "./prometheus";
+import {DataGrid} from "@material-ui/data-grid";
+import {getRangeOffset, Queries, rowsFromQueries} from "./prometheus";
 
 /**
  * A static array of column definitions for the Data Grid.
@@ -18,21 +18,23 @@ const columns = [{
 ];
 
 export default function HvacDataGrid(props) {
-    const { startDate, endDate, duration, resolution} = props;
-    const { range, offset } = getRangeOffset(startDate, endDate);
+    const { startDate, endDate, duration, resolution, errorCallback } = props;
 
     const [rows, setRows] = useState([]);
     const [loading, setLoading] = useState(null);
 
     useEffect(() => {
+        const { range, offset } = getRangeOffset(startDate, endDate);
         if (range <= 0 || offset < 0) return;
+
         setLoading(true);
         rowsFromQueries(range, offset, duration, resolution)
-            .then(rows => {
-                setRows(rows);
-                setLoading(false);
-            });
-    // eslint-disable-next-line
+            .then(setRows)
+            .catch(error => {
+                setRows([]);
+                errorCallback(error.message);
+            })
+            .finally(() => setLoading(false));
     }, [startDate, endDate, duration, resolution]);
 
     return (
