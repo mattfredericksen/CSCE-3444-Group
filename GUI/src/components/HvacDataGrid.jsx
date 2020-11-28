b2364d7ff9b8b13a3d0e59070485915061fb3b5b
mattfredericksen <mfred98@gmail.com>
2020-11-28 16:49:27 -0600

    Synchronized query result processing by adding timestamp IDs. Closes #24.

--- GUI/src/components/HvacDataGrid.jsx
+++ GUI/src/components/HvacDataGrid.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import moment from "moment";
 import {DataGrid} from "@material-ui/data-grid";
 import {getRangeOffset, Queries, rowsFromQueries} from "./prometheus";
@@ -22,29 +22,43 @@ export default function HvacDataGrid(props) {
 
     const [rows, setRows] = useState([]);
     const [loading, setLoading] = useState(null);
+    const queryID = useRef(moment(0));
 
     useEffect(() => {
         const { range, offset } = getRangeOffset(startDate, endDate);
         if (range <= 0 || offset < 0) return;
 
         setLoading(true);
+
+        // queryID.current will always contain the
+        // timestamp  of the most recent query
+        const effectID = moment();
+        queryID.current = effectID;
+
         rowsFromQueries(range, offset, duration, resolution)
             .then(rows => {
-                setRows(rows);
-                setAlert();
+                if (effectID.isSame(queryID.current)) {
+                    setRows(rows);
+                    setLoading(false);
+                    setAlert();
+                    setLoading(false)
+                }
             })
             .catch(error => {
-                setRows([]);
-                setAlert(error.message);
-            })
-            .finally(() => setLoading(false));
+                if (effectID.isSame(queryID.current)) {
+                    setRows([]);
+                    setAlert(error.message);
+                    setLoading(false)
+                }
+            });
     // eslint-disable-next-line
     }, [startDate, endDate, duration, resolution]);
 
     return (
         <div style={{ width: '100%', height: '200px' }}>
-            <DataGrid rows={rows} columns={columns} loading={loading}
-                      autoPageSize autoHeight
+            <DataGrid
+                rows={rows} columns={columns} loading={loading}
+                autoPageSize autoHeight rowsPerPageOptions={[5, 10, 25, 50, 100]}
             />
         </div>
     );
