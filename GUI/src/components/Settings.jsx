36e2247c14b14b4a1b7884f28a654df7f7ce8a25
mattfredericksen <mfred98@gmail.com>
2020-12-03 01:02:12 -0600

    Added email PUT request to Settings view for #6/#7.

--- GUI/src/components/Settings.jsx
+++ GUI/src/components/Settings.jsx
@@ -1,44 +1,50 @@
-import React, { Component } from "react";
+import React, {useCallback, useEffect, useRef, useState} from "react";
+import axios from 'axios';
+
+const api = "http://dodc:5000/"
 
 const divStyle = {
     color: '#ffffff',
     textAlign: "center",
 };
 
-class Settings extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            fname: "",
-            email: ""
-        }
-        this.handleChange = this.handleChange.bind(this);
-    }
-    mySubmitHandler = (event) => {
-        event.preventDefault();
-        alert("You are submitting "+ this.state.fname + " with email " + this.state.email);
-    }
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+export default function Settings() {
+    const [placeholder, setPlaceholder] = useState("Enter Email");
+    useEffect(() => {
+        fetch(api + 'email/')
+            .then(res => res.json())
+            .then(({email}) => {if (email) setPlaceholder(email)})
+            .catch(error => console.error(error));
+    }, []);
 
+    const email = useRef("");
+    const setEmail = useCallback(
+        (event) => email.current = event.target.value,
+        []
+    );
 
-    render() {
-        return (
-            <form style={divStyle} onSubmit={this.mySubmitHandler}>
-                <h3 style={divStyle}>
-                    Please enter name and email address to add.
-                </h3>
-                <input type="text" name="fname" placeholder="Enter Name" onChange={this.handleChange}/>
-                <br />
-                <input type="text" name="email" placeholder="Enter Email" onChange={this.handleChange}/>
-                <br />
-                <input type="submit" value="Submit"/>
-            </form>
-        );
-    }
+    const submitHandler = useCallback(
+        (event) => {
+            event.preventDefault();
+            axios.put(api + 'email/', {email: email.current})
+                .then(res => console.log(res))
+                .catch(e => {
+                    // perform some alerting here
+                    console.error(e);
+                });
+        }, []
+    );
+
+    return (
+        <form style={divStyle} onSubmit={submitHandler}>
+            <h3 style={divStyle}>
+                {"Please enter name and email address to add."}
+            </h3>
+            <input type={"email"} name={"email"} onChange={setEmail}
+                   placeholder={placeholder}/>
+            <br />
+            <input type={"submit"} value={"Submit"}/>
+        </form>
+    );
 }
-export default Settings
 
